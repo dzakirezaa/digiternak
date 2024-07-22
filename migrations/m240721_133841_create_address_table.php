1a73b92b3388aa17ad3aff32431c334a2cccffe7
unknown <pulungrafi@apps.ipb.ac.id>
2024-07-22 13:15:42 +0700

    update folder

---
+++ migrations/m240721_133841_create_address_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%address}}`.
+ */
+class m240721_133841_create_address_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%address}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%address}}');
+    }
+}
